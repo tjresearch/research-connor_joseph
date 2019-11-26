2af6bd6c9354d9bcc33fa10c7f11cceb467f76f6
Joseph Lee <jo112095@gmail.com>
2019-11-25 23:31:57 -0500

    Initial Upload

---
+++ resize_disaster_images.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from PIL import Image
+curr_dir = Path(".")
+
+
+def resize_images(directory, w, h):
+    """
+    :param directory: the path to the folder of images
+    :return:
+    """
+    pathoriginal = curr_dir / directory
+    pathresized = curr_dir / (directory + "_resized")
+    path_list = pathoriginal.glob("*")
+    i = 0
+    for c in path_list:
+        try:
+            im = Image.open(c, "r")
+            im = im.resize((w, h), Image.ANTIALIAS)
+            im = im.convert("RGB")
+            im.save(pathresized / f'{i}.jpg')
+            i += 1
+
+        except:
+            i += 1
+            continue
+        if i == 1001:
+            break
+
+
+resize_images('normal', 500, 500)
